b7c9fc5214b3b0e922513fd3640c94da9d1e6f60
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-02 15:53:24 +0200

    Implemented in-order depth-first search (DFS) for Binary Search Tree

--- binary-search-trees-exercises/index.js
+++ binary-search-trees-exercises/index.js
@@ -8,6 +8,9 @@ function BinarySearchTree(){
     this.root = null;
 }
 
+// DFSInOrder
+// This function should search through each node in the binary search tree using in-order depth first search and return an array containing each node's value.
+
 // DFSPreOrder
 BinarySearchTree.prototype.DFSPreOrder = function () {
     const array = []
@@ -25,3 +28,27 @@ BinarySearchTree.prototype.DFSPreOrder = function () {
 }
 
 // First we create a new array to store the nodes, then we create a function to implement the pre order dfs on every node so it has to work recursively (if there is no node we do nothing, otherwise we push the value of the node then execute the pre order function on the left node and then on the right node of the node I am on)
+
+
+
+// ############################
+
+// DFSInOrder
+// This function should search through each node in the binary search tree using in-order depth first search and return an array containing each node's value.
+
+BinarySearchTree.prototype.DFSInOrder = function () {
+    const array = []
+
+    function InOrderDFS(node) {
+        if(node === null) return
+
+        InOrderDFS(node.left)
+        array.push(node)
+        InOrderDFS(node.right)
+    }
+
+    InOrderDFS(this.root)
+    return array
+}
+
+// First we create a new array to store the nodes, then we create a function to implement the in order dfs on every node so it has to work recursively (if there is no node we do nothing, otherwise we execute the in order function on the left node and then we push the value of the node we are on and finally we execute the in order function on the right node of the node we are on)
