26f2402fa786e951cd92bf6b5df417fadad1cb63
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-15 20:16:25 +0200

    implemented two-sum solution

--- arrays-hashing-exercises/index.js
+++ arrays-hashing-exercises/index.js
@@ -68,3 +68,160 @@ var containsDuplicate = function(nums) {
 
     return false
 }
+
+
+// ####################
+// ####################
+
+
+// Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// First Solution:
+// The initial solution used a basic approach by sorting the strings,
+// resulting in a time complexity of O(n log n) due to the sorting algorithm.
+
+var isAnagram = function(s, t) {
+    
+    const sortedS = s.split("").sort().join("")
+    const sortedT = t.split("").sort().join("")
+    
+    return sortedS === sortedT
+};
+
+// Second Solution
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// Improved the solution to achieve a time complexity of O(n) by using
+// character frequency counting instead of sorting. This approach is more
+// efficient, especially for longer strings.
+
+
+var isAnagram = function(s, t) {
+    
+    if(s.length !== t.length)
+        return false
+    
+    const sCharCount = {}
+
+    for(const letter of s) {
+        if(!sCharCount[letter]) {
+            sCharCount[letter] = 1
+        }
+
+        sCharCount[letter]++
+    }
+
+    const tCharCount = {}
+
+    for(const letter of t) {
+        if(!tCharCount[letter]) {
+            tCharCount[letter] = 1
+        }
+
+        tCharCount[letter]++
+    }
+
+    for(const [key, value] of Object.entries(sCharCount)) {
+        if(!tCharCount[key] || tCharCount[key] !== sCharCount[key]) {
+            return false
+        }
+    }
+
+    return true
+};
+
+
+
+
+// #############
+// #############
+
+// Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+
+
+
+// Utilizes a hash map to store elements and their indices in a single pass.
+// Achieves a time complexity of O(n), where n is the length of the nums array.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const chars = {}
+    for(let i = 0; i < nums.length; i++) {
+        if(!chars[nums[i]]) {
+            chars[nums[i]] = i
+        }
+    }
+    
+    // Find the two numbers that add up to the target
+    for(let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if(chars[complement] && chars[complement] !== i) {
+            return [i, chars[complement]]
+        }
+    }
+
+    return null
+};
