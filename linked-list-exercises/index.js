1dbf1301c69798fe240de978d2951c220ce9e66a
mhmdhussein377 <mohammad.hussein377@gmail.com>
2023-11-19 18:27:57 +0200

    implement middleNode function for singly-linked lists

--- linked-list-exercises/index.js
+++ linked-list-exercises/index.js
@@ -148,3 +148,69 @@ var removeElements = function(head, val) {
 };
 
 
+
+// ############
+// ############
+
+
+// Middle of the Linked List
+
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 100].
+// 1 <= Node.val <= 100
+
+
+// SOLUTION
+
+/**
+ * Returns the middle node of a given singly-linked list.
+ *
+ * Uses the two-pointer approach with a slow and a fast pointer.
+ * The slow pointer moves one step at a time, while the fast pointer moves two steps.
+ * When the fast pointer reaches the end, the slow pointer is at the middle.
+ *
+ * Time Complexity: O(n), where n is the number of nodes in the linked list.
+ * The function traverses the list once, with both pointers covering the entire list.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = fast = head
+
+    while(fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    return slow
+};
